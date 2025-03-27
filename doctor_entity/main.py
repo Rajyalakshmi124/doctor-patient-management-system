a83ca25cbca692798e0c5dba0e074b0f651aa500
Rajyalakshmi Vemula <rajyalakshmi.vemula@theelixrlabs.com>
2025-03-27 15:45:39 +0530

    Initial commit

--- doctor_entity/main.py
+++ doctor_entity/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 # Pass the required route to the decorator. 
 @app.route("/") 
 def hello(): 
-    return "Hello, Welcome to Elixr"
+    return "Second POC"
   
 if __name__ == "__main__": 
     app.run(debug=True)
