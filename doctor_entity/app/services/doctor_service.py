e5169010bf69e8f70ce50871710ebed3fd6556a5
Rajyalakshmi Vemula <rajyalakshmi.vemula@theelixrlabs.com>
2025-04-01 13:10:48 +0530

    Removed unnecessary files

--- doctor_entity/app/services/doctor_service.py
+++ doctor_entity/app/services/doctor_service.py
@@ -17,25 +17,35 @@ class DoctorService:
             dept_name = data.get('department')
  
             # Validations
+            errors = []
             if not first_name or not last_name or not dept_name:
-                return {"error": "First name and last name and department are required"}, 400
+                errors.append("First name and last name and department name are required"), 400
            
-            if not first_name.isalpha() or not last_name.isalpha():
-                return {"error": "First name and last name must contain only letters"}, 400
+            if first_name and not first_name.isalpha():
+                errors.append("First name must contain only letters"), 400
             
-            if not dept_name.isalpha():
-                return{"error":"Department name must contain only letters"}, 400
+            if last_name and not last_name.isalpha():
+                errors.append("Last name must contain only letters"), 400
+            
+            if dept_name and not dept_name.isalpha():
+                errors.append("Department name must contain only letters"), 400
+
+            if errors:
+                return {"success": False, "errors": errors},400
+            
             # Call repository function to insert doctor into the database
             doctor_id = self.doctor_repo.add_doctor(first_name, last_name, dept_name)
             
             # Return success response with doctor details
-            return {"success": True, 
+            return {
+                "success": True, 
                     "doctor_id": doctor_id ,
                     "first_name":first_name,
                     "last_name":last_name, 
-                    "department":dept_name}, 201
+                    "department":dept_name
+            }, 201
         
         # Handle unexpected errors and return a server error response
         # e is an object of Exception
         except Exception as e:
-            return {"success": False,"error": str(e)}, 500
+            return {"success": False,"errors": [str(e)]}, 500
