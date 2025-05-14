6fc1790f6aa4e359f83c9202c6f769e2959314a2
Rajyalakshmi Vemula <rajyalakshmi.vemula@theelixrlabs.com>
2025-05-14 17:42:19 +0530

    Switched JWT secret key to environment variable

--- doctor_entity/main.py
+++ doctor_entity/main.py
@@ -1,22 +1,19 @@
 import os
-from dotenv import load_dotenv
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from app.controllers.doctor_controller import doctor_bp
 from app.controllers.patient_controller import patient_bp
 from app.controllers.auth_controller import auth_bp
 
-# Load environment variables from a .env file
-load_dotenv()
-
 # Initialize the flask application
 app = Flask(__name__) 
 app.json.sort_keys = False
 
 # JWT Configuration using secret key from environment
-app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET_KEY")
+app.config['JWT_SECRET_KEY'] = os.environ.get("JWT_SECRET_KEY")
 jwt = JWTManager(app)
-  
+print("jwt key:", app.config['JWT_SECRET_KEY'])
+
 # Pass the required route to the decorators
 app.register_blueprint(doctor_bp)
 app.register_blueprint(patient_bp)
